6b7b6e0ab7e344f025d2980938289885968351c5
Lucas Carvalho <lucas.carvalho@snowfox-ai.com>
2025-01-21 18:47:22 -0500

    feature: add get_delimiter function to read csv; fix paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,17 @@
+import csv
 import pickle
 
 import pandas as pd
 import pytest
 
 from typing import Any
+
+
+def get_delimiter(file_path: str) -> str:
+    sniffer = csv.Sniffer()
+    data = open(file_path, "r").read(4096)
+    delimiter = sniffer.sniff(data).delimiter
+    return delimiter
 
 
 def read_pickle(file_path: str) -> Any:
@@ -14,9 +22,10 @@ def read_pickle(file_path: str) -> Any:
 
 @pytest.fixture
 def training_data() -> pd.DataFrame:
-    return read_pickle('./data/processed/training_data.pkl')
+    return read_pickle('./tests/data/processed/training_data.pkl')
 
 
 @pytest.fixture
 def data() -> pd.DataFrame:
-    return pd.read_csv('./data/raw/data.csv')
+    delimiter = get_delimiter('./tests/data/raw/data.csv')
+    return pd.read_csv('./tests/data/raw/data.csv', delimiter=delimiter)
