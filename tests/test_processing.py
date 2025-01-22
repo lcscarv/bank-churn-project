d381c3f579185a2c4931817b5ba3da13ffb71d41
Lucas Carvalho <lucas.carvalho@snowfox-ai.com>
2025-01-21 19:38:48 -0500

    feature: create test to obtain_most_recent_file function

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,7 +1,12 @@
 import pandas as pd
 
+from finance_churn_project.processing.data_loader import obtain_most_recent_file
 from finance_churn_project.processing.data_processing import processing_pipeline
 
 
 def test_processing_pipeline(data: pd.DataFrame) -> pd.DataFrame:
     return processing_pipeline(data)
+
+
+def test_obtain_most_recent_file() -> str:
+    return obtain_most_recent_file(folder_path='./tests/data/raw/')
